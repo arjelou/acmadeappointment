4cb7d822043d9c8a042dc9c8759dcf12511a8dc0
Jelou <arjelou.jelou@gmail.com>
2023-03-13 17:57:33 +0800

    ACMDE Appointment Schedule Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaCalendarAlt, FaTrashAlt } from 'react-icons/fa';
+import appointmentList from './data.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h2><FaCalendarAlt color='blue' />ACMADE Appointment</h2>
+      </div>
+      <div>
+        <form>
+          <div class="mb-3 mt-5">
+            <label for="exampleInputEmail1" class="form-label">Pet Name</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Owner Name</label>
+            <input type="text" class="form-control" id="exampleInputPassword1" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Appointment Notes</label>
+            <textarea typeof='text' class='form-control' />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Appointment Date</label>
+            <input type="date" class="form-control" id="exampleInputPassword1" />
+          </div>
+          <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+      </div>
+      <div>
+      <ul class="list-group list-group-flush">
+       {
+        appointmentList.map((appt) => (
+          <> 
+            <li class="list-group-item">Name: {appt.petName}
+            <p>owner Name: {appt.ownerName}</p>
+            <span>Notes: {appt.aptNotes}</span>< br />
+            <span>Date: {appt.aptDate}</span>
+            <button><FaTrashAlt color='red'/></button>
+            </li>
+            
+          </>
+          
+        ))
+       }
+      </ul>
+      </div>
     </div>
   );
 }
